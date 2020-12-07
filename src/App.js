63165709bfcc1bd86d67609bab911e14ecdfc2f6
xumaozeng <15611895341@163.com>
2020-12-07 09:37:15 +0800

    [REM] 多余的文件

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
 import React from "react";
 import MyRCForm from "./pages/MyRCForm";
-// import HocPage from "./pages/HocPage";
-// import Toggle from "./pages/Toggle";
-// import Clock from "./pages/Clock";
-// import DialogPage from "./pages/DialogPage";
-// import TickClockPage from "./pages/TickClockPage";
-// import UseCallbackPage from "./pages/UseCallbackPage";
-// import UseMemoPage from "./pages/UseMemoPage";
-// import CustomHookPage from "./pages/CustomHookPage";
-// import HookPage from "./pages/HookPage";
-// import ReactReduxPage from "./pages/ReactReduxPage";
-// import ReduxPage from "./pages/ReduxPage";
-// import ClassComponent from "./pages/ClassComponent";
-// import FunctionComponent from "./pages/FunctionComponent";
-// import HelloMessage from "./pages/HelloMessage ";
-// import HomePage from "./pages/HomePage";
-// import UserPage from "./pages/UserPage";
-// import SetStatePage from "./pages/SetStatePage";
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassComponent /> */}
-      {/* <FunctionComponent /> */}
-      {/* <SetStatePage /> */}
-      {/* <HelloMessage name="徐茂增" /> */}
-      {/* <HomePage /> */}
-      {/* <UserPage /> */}
-      {/* <ReduxPage /> */}
-      {/* <ReactReduxPage /> */}
-      {/* <HookPage /> */}
-      {/* <CustomHookPage /> */}
-      {/* <UseMemoPage /> */}
-      {/* <UseCallbackPage /> */}
-      {/* <TickClockPage /> */}
-      {/* <DialogPage /> */}
-      {/* <Clock /> */}
-      {/* <Toggle /> */}
-      {/* <HocPage /> */}
       <MyRCForm />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import logo from "./logo.svg";
 // import styles from "./index.module.scss"; // 模块化
-// import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -15,40 +13,6 @@ import store from "./store";
  * babel-loader把JSX编译成相应的JS对象
  * React.createElement再把这个JS对象构造成React需要的虚拟dom
  */
-// 基本使用表达式用{}包裹
-// const name = "React";
-// const obj = {
-//   name: "xiaoxu",
-//   age: 25
-// };
-// // 函数
-// function formatName(obj) {
-//   return obj.name + "的年龄是" + obj.age;
-// }
-// // JSX对象
-// const great = <div>good</div>;
-// // 条件语句
-// const show = true;
-// // 数组
-// const a = [1, 2, 3];
-
-// const jsx = (
-//   <div className={styles.app}>
-//     <div>Hello, {name}</div>
-//     <div>{formatName(obj)}</div>
-//     {great}
-//     {show ? great : "登录"}
-//     {show && great}
-//     <ul>
-//       {/* diff时候，首先比较type,然后是key,所以同级同类型元素
-//       key必须得唯一 */}
-//       {a.map(item => (
-//         <li key={item}>{item}</li>
-//       ))}
-//     </ul>
-//     <img src={logo} alt="logo" className={styles.logo} />
-//   </div>
-// );
 
 ReactDOM.render(
   <Provider store={store}>
@@ -56,8 +20,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
