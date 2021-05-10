e4ffd5a72ffa2e0c84f08c220ebe7914f7442518
xumaozeng <15611895341@163.com>
2021-05-10 23:08:55 +0800

    html

--- src/pages/DL/index.js
+++ src/pages/DL/index.js
@@ -3,21 +3,33 @@ import data from "./data";
 import "./index.css";
 import { useState } from "react";
 
+const htmlData = `
+<h2>React修炼</h2>
+<p>从零开始学习</p>
+`;
+
 function DL() {
   const [openName, setOpenName] = useState("");
 
   return (
-    <div className="friend-list">
-      {Object.keys(data).map(itemName => (
-        <DlPage
-          name={itemName}
-          openName={openName}
-          changeOpen={setOpenName}
-          key={itemName}
-          dlData={data[itemName]}
-        />
-      ))}
-    </div>
+    <>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: htmlData
+        }}
+      ></div>
+      <div className="friend-list">
+        {Object.keys(data).map(itemName => (
+          <DlPage
+            name={itemName}
+            openName={openName}
+            changeOpen={setOpenName}
+            key={itemName}
+            dlData={data[itemName]}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 export default DL;
